d1ec60719ef576aa9a6339e52cbaf5c97d7ea743
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-08-21 16:14:37 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
 
         Scanner input = new Scanner(System.in);
         String statement = input.nextLine();
 
         while (!statement.equals("")) {
             if(statement.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("     Bye. Hope to see you again soon!");
                 break;
             }
             else {
-                System.out.println(statement);
+                System.out.println("     " + statement);
                 statement = input.nextLine();
             }
         }
