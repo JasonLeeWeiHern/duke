5c65c50be7920291dfdf17f9464026e70c411f10
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-08-27 16:23:14 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
 
-        System.out.println("     Hello! I'm Duke");
-        System.out.println("     What can I do for you?");
+        String[] arr = new String[100];
+        int position = 0;
 
         Scanner input = new Scanner(System.in);
         String statement = input.nextLine();
 
         while (!statement.equals("")) {
+            int list_number = 1;
             if(statement.equals("bye")) {
-                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("\tBye. Hope to see you again soon!");
                 break;
             }
+            if(statement.equals("list")) {
+                for(int i = 0; i < position; i++) {
+                    System.out.println("\t" + list_number + ". " + arr[i]);
+                    list_number++;
+                }
+            }
             else {
-                System.out.println("     " + statement); //print out a statement
+                System.out.println("\tadded: " + statement);
+                arr[position] = statement;
+                position++;
             }
             statement = input.nextLine();
         }
