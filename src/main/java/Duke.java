f42a80b43ca3fa475f18b93ecaa1c0cb060c1d1f
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-08-28 02:33:23 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,31 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
 
-        String statement = input.nextLine(); //read an input
-
-        while (!statement.equals("")) {
-            Task newTask = new Task(statement); //assign the statement to the task
-            String[] splitString = statement.split(" ");//split the string into individual words
+        while (true) {
+            String statement;
+            String[] splitString;
+            while(true) {
+                statement = input.nextLine(); //read an input
+                Task newTask = new Task(statement); //assign the statement to the task
+                splitString = statement.split(" ");//split the string into individual words
+                try {
+                    if(statement.equals("todo") || statement.equals("deadline") || statement.equals("event")) {
+                        throw new DukeException("\t☹ OOPS!!! The description of a " + splitString[0] + " cannot be empty.");
+                    }
+                    else if(!statement.equals("list") && !(splitString[0].equals("done") && splitString.length != 1) && !(statement.equals("bye")) &&
+                            !splitString[0].equals("todo") && !splitString[0].equals("deadline") && !splitString[0].equals("event")) {
+                        throw new DukeException("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                    else {
+                        break;
+                    }
+                }
+                catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
             String agenda = "";
-            String time = "";
+            String time = ""; //for the date and time
             int list_number = 1;
             if(statement.equals("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
@@ -41,43 +59,40 @@ public class Duke {
             }
 
             else if (splitString[0].equals("todo")) {
-                for(int i = 1; i < splitString.length; i ++) {
-                    agenda += splitString[i] + " ";
-                }
-                ToDo todo = new ToDo(agenda);
-                arr.add(todo);
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t  " + todo.getDescription());
-                System.out.println("\tNow you have " + arr.size() +  " tasks in the list.");
-
-            }
-            else if(splitString[0].equals("deadline")) {
-                for(int i = 1; i < splitString.length; i++) {
-                    if(!splitString[i].equals("/by")) {
+                    for (int i = 1; i < splitString.length; i++) {
                         agenda += splitString[i] + " ";
                     }
-                    else {
-                        while((i+1) < splitString.length) {
-                            time += " " + splitString[i+1];
-                            i += 1;
+                    ToDo todo = new ToDo(agenda);
+                    arr.add(todo);
+                    System.out.println("\tGot it. I've added this task:");
+                    System.out.println("\t  " + todo.getDescription());
+                    System.out.println("\tNow you have " + arr.size() + " tasks in the list.");
+            }
+            else if(splitString[0].equals("deadline")) {
+                    for (int i = 1; i < splitString.length; i++) {
+                        if (!splitString[i].equals("/by")) {
+                            agenda += splitString[i] + " ";
+                        } else {
+                            while ((i + 1) < splitString.length) {
+                                time += " " + splitString[i + 1];
+                                i += 1;
+                            }
+                            break;
                         }
-                        break;
                     }
-                }
-
-                Deadline newDeadline = new Deadline(agenda, time);
-                arr.add(newDeadline);
-                System.out.println("\tGot it. I've added this task:");
-                System.out.println("\t  " + newDeadline.getDescription());
-                System.out.println("\tNow you have " + arr.size() +  " tasks in the list.");
+                    Deadline newDeadline = new Deadline(agenda, time);
+                    arr.add(newDeadline);
+                    System.out.println("\tGot it. I've added this task:");
+                    System.out.println("\t  " + newDeadline.getDescription());
+                    System.out.println("\tNow you have " + arr.size() + " tasks in the list.");
             }
             else if(splitString[0].equals("event")) {
-                for(int i = 1; i < splitString.length; i++) {
+                for (int i = 1; i < splitString.length; i++) {
                     if (!splitString[i].equals("/at")) {
                         agenda += splitString[i] + " ";
                     } else {
                         while ((i + 1) < splitString.length) {
-                            time +=  " " + splitString[i + 1];
+                            time += " " + splitString[i + 1];
                             i += 1;
                         }
                         break;
@@ -87,15 +102,8 @@ public class Duke {
                 arr.add(newEvent);
                 System.out.println("\tGot it. I've added this task:");
                 System.out.println("\t  " + newEvent.getDescription());
-                System.out.println("\tNow you have " + arr.size() +  " tasks in the list.");
+                System.out.println("\tNow you have " + arr.size() + " tasks in the list.");
             }
-            else {
-                System.out.println("\tadded: " + statement);
-                arr.add(newTask);
-                position++;
-            }
-            statement = input.nextLine();
         }
     }
-
 }
