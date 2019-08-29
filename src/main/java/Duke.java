05599d7bddfdad78815d47ca2d41cbdf16724a33
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-08-29 20:27:16 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,27 +6,39 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
 
-        String[] arr = new String[100];
-        int position = 0;
+        ArrayList<Task> arr = new ArrayList<Task>(); //initialise ArrayList
+
+        int position = 0; //Index in the ArrayList
 
         Scanner input = new Scanner(System.in);
-        String statement = input.nextLine();
+
+        String statement = input.nextLine(); //read an input
 
         while (!statement.equals("")) {
+            Task newTask = new Task(statement); //assign the statement to the task
+            String[] splitString = statement.split(" ");//split the string into individual words
             int list_number = 1;
             if(statement.equals("bye")) {
                 System.out.println("\tBye. Hope to see you again soon!");
                 break;
             }
-            if(statement.equals("list")) {
+
+            else if(statement.equals("list")) {
+                System.out.println("\tHere are the tasks in your list:");
                 for(int i = 0; i < position; i++) {
-                    System.out.println("\t" + list_number + ". " + arr[i]);
+                    System.out.println("\t" + list_number + "." + "[" + arr.get(i).getStatusIcon() + "] " + arr.get(i).getDescription());
                     list_number++;
                 }
             }
+            else if(splitString[0].equals("done")) {
+                System.out.println("\tNice! I've marked this task as done:");
+                int num_to_tick = Integer.parseInt(splitString[1]) - 1; //convert the string to number
+                arr.get(num_to_tick).markasDone();
+                System.out.println("\t  [" + arr.get(num_to_tick).getStatusIcon() + "] " + arr.get(num_to_tick).getDescription());
+            }
             else {
                 System.out.println("\tadded: " + statement);
-                arr[position] = statement;
+                arr.add(newTask);
                 position++;
             }
             statement = input.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String var1) {
+        this.description = var1;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markasDone() {
+        this.isDone = true;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
