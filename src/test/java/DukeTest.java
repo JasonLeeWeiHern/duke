06f25ac7c70f72199876a5de64fc5144eabcf4b0
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-09-12 18:16:26 +0800

    Added CDF test

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -3,32 +3,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     Deadline testD = new Deadline("borrow books ", "12/12/2019 1900");
+
     @Test
     void testDeadlinegetD() {
-        assertEquals(testD.getDescription(), "[D][\u2718] borrow books (by:12/12/2019 1900)");
+        assertEquals("[D][\u2718] borrow books (by:12/12/2019 1900)", testD.getDescription());
     }
     @Test
     void testDeadlinegetF() {
-        assertEquals(testD.getFormat(),"D | 0 | borrow books |12/12/2019 1900");
+        assertEquals("D | 0 | borrow books |12/12/2019 1900",testD.getFormat());
     }
 
     ToDo testT = new ToDo("borrow books");
     @Test
     void testToDogetD() {
-        assertEquals(testT.getDescription(),"[T][\u2718] borrow books");
+        assertEquals("[T][\u2718] borrow books",testT.getDescription());
     }
     @Test
     void testToDogetF() {
-        assertEquals(testT.getFormat(),"T | 0 | borrow books");
+        assertEquals("T | 0 | borrow books",testT.getFormat());
     }
 
     Event testE = new Event("borrow books ", "12/12/2019 1900");
     @Test
     void testEventgetD() {
-        assertEquals(testE.getDescription(), "[E][\u2718] borrow books (at:12/12/2019 1900)");
+        assertEquals("[E][\u2718] borrow books (at:12/12/2019 1900)", testE.getDescription());
     }
     @Test
     void testEventgetF(){
-        assertEquals(testE.getFormat(),"E | 0 | borrow books |12/12/2019 1900");
+        assertEquals("E | 0 | borrow books |12/12/2019 1900",testE.getFormat());
     }
+
+    ChangeDateFormat testcdf = new ChangeDateFormat("12/12/2019 1930");
+    @Test
+    void testCDF() {
+        assertEquals("12th of December 2019, 7.30pm", testcdf.NewFormat("12/12/2019 1930"));
+    }
+
+
 }
